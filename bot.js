8c4f44bb3a589dcf48f9979dc37e1d7e74fcbae2
JOKER_SER <87939150+J0kerser@users.noreply.github.com>
2022-07-27 18:59:12 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,17 +33,17 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.owner = ['919446697883',''] //owner number +27634090203
+global.wame = ['919446697883'] 
+global.pemilik = ['919446697883'] //owner number +27634090203
+global.premium = ['919446697883'] //premium number +27634090203
+global.pengguna = 'Jokerser' //username
+global.footer = 'Joker Ser '
+global.botnma = 'ZIM BOT' //bot name
+global.botname = 'ZIM BOT '
+global.ownernma = 'Jokerser' //owner name
+global.packname = 'while (no success) {             tryagain()          If (dead)                        Break; }' //sticker package name
+global.author = 'By ᴊᴏᴋᴇʀ sᴇʀꥯ' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
@@ -72,15 +72,15 @@ global.f5 = 'application/pdf'
 global.f6 = 'application/vnd.android.package-archive'
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
 global.mess = {
-    success: 'Takaenda Done!',
+    success: 'SET MONU!',
     admin: '*This feature is only for admin!*',
     botAdmin: '*zim bot  must be admin first!*',
-    owner: '*this feature is only for zimbot owner*',
+    owner: '*this feature is only for bot owner*',
     group: '*Feature is only for groups!*',
     grouponly: '*Group Mode Only*',
     private: 'Features Used Only For Private Chat!',
     bot: 'This feature in only for the bot number',
-    wait: '*whoa wait a moment zim bot proccessing*',
+    wait: '*whoa wait a moment  bot proccessing*',
     endLimit: '*Your daily limit has expired the limit will be reseted every 12 hours*',
     noPetualang: '*you are not in rpg database type #joinrpg* _ur name_'
 }
